115c0c998fe5c7886afe2e68cf69bab46fbeaa66
Екатерина Савченко <KattySavchenko1@yandex.ru>
2023-07-29 17:21:18 +0200

    Тест деплоя, polling

--- db/sqlite_db.py
+++ db/sqlite_db.py
@@ -3,7 +3,7 @@ import sqlite3
 
 def start_sql():
     global base, cur
-    base = sqlite3.connect('kids_ai.db')
+    base = sqlite3.connect('db.sqlite3')
     cur = base.cursor()
     base.execute('CREATE TABLE IF NOT EXISTS info(name TEXT, content TEXT)')
     base.commit()
